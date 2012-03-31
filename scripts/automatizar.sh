1a9d68277ef367d303c81bcdae4fe73a7a7eb5f9
Anderson Rosa <andersonrosa8@gmail.com>
2012-03-31 18:51:44 -0300

    Organizando automatizar.sh

--- scripts/automatizar.sh
+++ scripts/automatizar.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+#Função enter: para deixar a execução do programa mais amigável
+enter(){
+	echo ""
+	echo "Tecle [Enter] para avançar..."
+	echo ""
+	read enter
+}
+
 #Função auxiliar: listar os arquivos especiais (named pipes e unix sockets)
 arquivosEspeciais()
 {
@@ -7,18 +15,23 @@ arquivosEspeciais()
 	find / -type p -ls 2> /dev/null | more
 	echo ""
 	echo "Agora, os arquivos do tipo 'unix socket'. Mais uma vez, aguarde..."
-	echo "Tecle [Enter] para avançar..."
-	read enter
+	enter
 	find / -type s -ls 2> /dev/null | more
+}
+
+#Função serviço: lista os serviços conforme seu nome, dono, protocolo ou porta
+servico(){
+	lsof -n -P | grep TCP | grep ${1}
+	lsof -n -P | grep UDP | grep ${1}
 }
 
 #Função principal: menu interativo
 while true
 do
 	clear
-	echo "Opção1:"
+	echo "Opções:"
 	echo "1) listar arquivos especiais: 'named pipes' e 'unix sockets'."
-	echo "2) listar serviços."
+	echo "2) listar serviços (Sockets)."
 	echo "3) sair do programa."
 	echo ""
 	echo -n "Escolha uma opção pelo seu número: "
@@ -30,10 +43,10 @@ do
 		"2")
 			echo "Lista serviços por nome de processo, dono ou porta?"
 			echo "1) Listar serviços por nome de processo."
-			echo "2) listar serviços por dono."
-			echo "3) listar serviços por protocolo."
-			echo "4) Encerrar um processo."
-			echo "5) listar serviço por porta."
+			echo "2) Listar serviços por dono."
+			echo "3) Listar serviços por protocolo."
+			echo "4) Listar serviço por porta."
+			echo "5) Encerrar um processo pelo seu numero."			
 			echo -n "Número: "
 			read opcao2
 			case ${opcao2} in
@@ -41,16 +54,14 @@ do
 					echo -n "Digite o nome do serviço que deseja listar: "
 					read nome
 					#grep (filtra linhas) e o (cut filtra coluna)
-					lsof -n -P | grep TCP | grep LISTEN | grep ${nome}
-					lsof -n -P | grep UDP | grep LISTEN | grep ${nome}
-					echo "Tecle [Enter] para avançar..."
-					read enter
+					servico ${nome}
+					enter
 					;;
 				"2")
 					echo -n "Digite o nome do usuário: "
 					read usuario
-					lsof -n -P | grep TCP | grep LISTEN | grep ${usuario}
-					lsof -n -P | grep UDP | grep LISTEN | grep ${usuario}
+					servico ${usuario}
+					enter
 					;;
 				"3")
 					echo -n "Digite o nome do protocolo que deseja listar: "
@@ -63,17 +74,20 @@ do
 					then
 						lsof -n | grep UDP | cut -d \  -f 1
 					fi
+					enter
 					;;
 				"4")
-					echo -n "Digite o nome do processo que você deseja encerrar: "
-					read nomeprocesso
-					killall -u ${nomeprocesso}
-					;;
-				"5")
 					echo -n "Digite o número da porta: "
 					read porta
-					lsof -n -P | grep TCP | grep LISTEN | grep ${porta}
-					lsof -n -P | grep UDP | grep LISTEN | grep ${porta}
+					servico ${porta}
+					enter
+					;;
+				"5")	
+					ps -aux | more
+					echo -n "Digite o número do PID do processo: "
+					read numprocesso
+					killall -9 ${numprocesso}
+					enter
 					;;
 				*)
 					echo "Por favos, leia com atenção, digite um numero válido!"
