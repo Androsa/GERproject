a389f6076fcb739b8235d3ebe8f1037c2e72eb3d
Ricardo Martins <ricardo.ifsc@gmail.com>
2012-04-03 16:38:07 -0300

    Modificado ciclo.sh para aceitar linha de comando

--- scripts/ciclo.sh
+++ scripts/ciclo.sh
@@ -4,6 +4,14 @@ then
 	echo -e "\033[44;37;1mEste programa executou uma operação ilegal e será fechado. Motivo: Você é root!\033[m"
 	exit
 fi
+
+help ()	 {
+	echo "[c] - Cria um arquivo."
+	echo "[e] - Editar um arquivo."
+	echo "[:] - Abre linha de comando."
+	echo "[exit] - Sair do programa sem salvar"
+	echo "[help] - Lista os comandos válidos"
+	}
 criar () {
 	echo -n "Nome do arquivo: "
 	read nome
@@ -16,6 +24,8 @@ editar () {
 	gedit $arquivo
 	}
 
+clear
+echo "Baixando arquivos do github"
 git pull
 while true
 do
@@ -23,6 +33,7 @@ do
 	echo -e -n " para criar ou \033[41;37;1m[e]\033[m\c"
 	echo " para editar um arquivo."
 	echo "Se não digitar nada vai enviar o(s) arquivo(s)"
+	echo "Digite help para ajuda."
 	read opcao
 	case $opcao in
 		"c")
@@ -36,6 +47,20 @@ do
 			git commit -a
 			git push
 			echo "comentario adicionado e enviado"
+			exit
+			;;
+		":")
+			while true
+			do
+			echo -n "> "
+			read comando
+			$comando
+			done
+			;;
+		"help")
+			help
+			;;
+		"exit")
 			exit
 			;;
 		*)
