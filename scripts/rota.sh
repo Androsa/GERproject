5d179f2388af9ae24cb00c59afad49bd3435bb0f
Ricardo Martins <ricardo.ifsc@gmail.com>
2012-06-01 15:16:02 -0300

    Mais coisa da rota.sh

--- scripts/rota.sh
+++ scripts/rota.sh
@@ -9,16 +9,17 @@ fi
 dns () {
 	mv /etc/resolv.conf /etc/resolv.conf.original
 	echo domain rtfm.com.br > /etc/resolv.conf
-	echo search rtfm.com.br
-	echo nameserver 192.168.2.7
-	echo nameserver 192.168.2.8
+	echo search rtfm.com.br >> /etc/resolv.conf
+	echo nameserver 192.168.2.7 >> /etc/resolv.conf
+	echo nameserver 192.168.2.8 >> /etc/resolv.conf
+	}
 
 	echo -n "Digite '1' para configurar rotas no notebook Sim,"
 	echo -n " '2' para configurar rotas no notebook Lenovo "
 	echo -n "ou '3' para desfazer a configuração do dns: "
 	read operacao
 
-case "${operacao}" in
+case $operacao in
 	"1")
 		ip1=$(/sbin/ifconfig eth0 | grep 'inet end' | awk '{print $3}')
 		route add -net 192.168.0.0/24 gw 10.0.0.1
