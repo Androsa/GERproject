4b47994eef4ce512246068d4190c952372c69bca
Ricardo Martins <ricardo.ifsc@gmail.com>
2012-09-21 16:50:37 -0300

    Funcionando roundcube e cacti.

--- lab_matriz/DMZservidor1-auto.sh
+++ lab_matriz/DMZservidor1-auto.sh
@@ -12,11 +12,7 @@ if [ -d "/hostlab/DMZservidor1/preserve" ]; then
 fi
 cp -p /hostlab/shared/templates/K95preserve /etc/rc0.d
 echo -n > /etc/default/preserve
-echo "/etc" >> /etc/default/preserve
-echo "/var/www" >> /etc/default/preserve
-/etc/init.d/cron start
-/etc/init.d/ssh start
-/etc/init.d/ntp start
-/etc/init.d/postfix start
+echo "/etc/bind" >> /etc/default/preserve
+echo "/etc/rc.local" >> /etc/default/preserve
 sysctl -w net.ipv4.ip_forward=1
 echo COMPLETE > /proc/mconsole
